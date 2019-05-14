8e2ae43fac5eb00923e5ac1ce530cfca2d2797fb
ravi <ravi.sharma@javra.com>
2019-05-14 14:58:21 +0545

    Lesson 2

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        About page
-    </div>
+        <h1 class="font-normal text-3xl text-grey-darkest leading-none">About page</h1>
 </template>
 
 <script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+
+</head>
+<body class="font-sans">
+<div id="app">
+    <div class="container mx-auto">
+        <header class="py-6 mb-8">
+            <h1>
+                <img src="/images/logo.svg" alt="Laracasts">
+            </h1>
+        </header>
+        <main class="flex">
+            <aside class="w-1/5">
+                <section class="mb-6">
+                    <h5 class="uppercase font-bold mb-4">The Brand</h5>
+                    <ul class="list-reset">
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="about">Logo</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo Symbol</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Colors</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Typography</router-link></li>
+                    </ul>
+                </section >
+                <section>
+                    <h5 class="uppercase font-bold mb-4">Doodles</h5>
+                    <ul class="list-reset">
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="about">Mascot</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Illustrations</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Loaders and Animations</router-link></li>
+                        <li class="text-sm leading-loose"><router-link class="text-black" to="/">Wallpapers</router-link></li>
+                    </ul>
+                </section>
+            </aside>
+            <div class="primary flex-1">
+                <router-view></router-view>
+            </div>
+        </main>
+
+    </div>
+</div>
+<script src="/js/app.js"></script>
+</body>
+</html>
