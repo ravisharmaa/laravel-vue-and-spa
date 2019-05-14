85bad0bc19c1055f0f9722a7e1b630e46a76a593
ravi <ravi.sharma@javra.com>
2019-05-14 13:07:32 +0545

    Initial Release

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Home page
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Home Component mounted.')
+        }
+    }
+</script>
